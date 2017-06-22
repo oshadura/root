c865da737594acfc894f57d92bd999a7373af6f1
Oksana Shadura <oksana.shadura@cern.ch>
2017-06-22 20:14:58 +0200

    Enabling LZ4 as default compression algorithm for stress test

--- test/stress.cxx
+++ test/stress.cxx
@@ -1216,7 +1216,7 @@ void stress10()
       snprintf(filename,20,"Event_%d.root",file);
       chfile[file] = new TFile(filename,"recreate");
       if (file>=5) {
-         chfile[file]->SetCompressionAlgorithm(ROOT::kLZMA);
+         chfile[file]->SetCompressionAlgorithm(ROOT::kLZ4);
       }
       chTree[file] = (TTree*)tree->CloneTree(0);
    }
