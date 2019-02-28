536f9cb585712003748da256fabd83d2d883e9d5
Oksana Shadura <oksana.shadura@cern.ch>
2019-02-28 16:41:52 +0100

    Snappy WIP

---
+++ core/snappy/src/ZipSnappy.cxx
@@ -0,0 +1,20 @@
+// Original Author: Oksana Shadura
+
+/*************************************************************************
+ * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "ZipSnappy.h"
+#include "ROOT/RConfig.hxx"
+
+void R__zipSnappy(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep)
+{
+}
+
+void R__unzipSnappy(int *srcsize, unsigned char *src, int *tgtsize, unsigned char *tgt, int *irep)
+{
+}
