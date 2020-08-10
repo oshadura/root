7747e728df95c0b2e47ed30ec419629385284965
Oksana Shadura <oksana.shadura@cern.ch>
2020-08-10 14:21:17 +0200

    Add ROOT header to zfp ROOT object library

--- core/zfp/inc/ZipZFP.h
+++ core/zfp/inc/ZipZFP.h
@@ -1,3 +1,13 @@
+// Author:
+
+/*************************************************************************
+ * Copyright (C) 1995-2020, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 #ifndef ROOT_ZipZFP
 #define ROOT_ZipZFP
 
