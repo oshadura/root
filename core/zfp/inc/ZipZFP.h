e908f9e4222412548f5230ab1da2a5e663edb36b
Skelpdar <skelpdar@gmail.com>
2020-08-10 10:56:01 +0200

    Added skeleton for zfp external in core

---
+++ core/zfp/inc/ZipZFP.h
@@ -0,0 +1,11 @@
+#ifndef ROOT_ZipZFP
+#define ROOT_ZipZFP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void R__zipZFP();
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ core/zfp/src/ZipZFP.cxx
@@ -0,0 +1,12 @@
+#include "ZipZFP.h"
+
+#include "ROOT/RConfig.hxx"
+
+#include <zfp.h>
+
+void R__zipZSTD(){
+
+	zfp_type type = zfp_type_double;
+	zfp_stream* zfp = zfp_stream_open(NULL);
+
+}
